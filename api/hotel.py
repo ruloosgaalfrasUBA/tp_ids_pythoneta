f797c518ed44d7499c06886b44feed4d8508912f
Lucas Tomas Kriger <lkriger@fi.uba.ar>
2024-11-06 23:20:09 -0300

    id: #10 Se agregó el listado de servicios a utilizar en hotel.py

---
+++ api/hotel.py
@@ -0,0 +1,14 @@
+# Listado de servicios a utilizar:
+
+# Buscar hoteles por estrellas
+# Buscar hoteles por fechas
+# Buscar hoteles por ubicación
+
+# Hacer una Reserva
+# Consultar Reserva
+# Modificar Reserva
+# Eliminar Reserva
+
+# Contratar Servicio
+# Cancelar Servicio
+# Listar Servicios
