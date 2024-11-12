c177827d68a0df9586145fb67191433f2770de61
MatiasDFFIUBA <mdomine@fi.uba.ar>
2024-11-12 11:17:16 -0300

    hoteles_querys

---
+++ app.py
@@ -0,0 +1,61 @@
+from flask import Flask, jsonify, request
+
+import hoteles
+
+app = Flask(__name__)
+
+@app.route('/api/v1/hoteles', methods=['GET'])
+def get_all_hoteles():
+    try:
+        result = hoteles.all_hoteles()
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+    response = []
+    for row in result:
+        response.append({'id_hotel': row[0], 'nombre': row[1], 'descripcion': row[2], 'provincia': row[3], 'estrellas':row[4]})
+
+    return jsonify(response), 200
+
+
+@app.route('/api/v1/hoteles/<int:id_hotel>', methods=['GET'])
+def get_by_id_hotel(id_hotel):
+    try:
+        result = hoteles.hotel_by_id(id_hotel)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+    if len(result) == 0:
+        return jsonify({'error': 'No se encontró el hotel'}), 404 # Not found
+
+    result = result[0]
+    return jsonify({'id_hotel': result[0], 'nombre': result[1], 'descripcion': result[2], 'provincia': result[3], 'estrellas': result[4]}), 200
+
+@app.route('/api/v1/hoteles/<provincia>', methods=['GET'])
+def get_by_provincia(provincia):
+    try:
+        result = hoteles.hoteles_provincia(provincia)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+    response = []
+    for row in result:
+        response.append({'id_hotel': row[0], 'nombre': row[1], 'descripcion': row[2], 'provincia': row[3], 'estrellas':row[4]})
+
+    return jsonify(response), 200
+
+@app.route('/api/v1/hoteles/estrellas:<int:estrellas>', methods=['GET'])
+def get_by_estrellas(estrellas):
+    try:
+        result = hoteles.hoteles_estrellas(estrellas)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+    response = []
+    for row in result:
+        response.append({'id_hotel': row[0], 'nombre': row[1], 'descripcion': row[2], 'provincia': row[3], 'estrellas':row[4]})
+
+    return jsonify(response), 200
+
+if __name__ == "__main__":
+    app.run("127.0.0.1", port="5000", debug=True)
---
+++ hoteles.py
@@ -0,0 +1,65 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy import text
+from sqlalchemy.exc import SQLAlchemyError
+
+QUERY_SELECT_HOTELES = "SELECT * FROM hotel ORDER BY id_hotel"
+
+QUERY_SELECT_HOTELES_ID = "SELECT * FROM hotel WHERE id_hotel = :id_hotel ORDER BY id_hotel "
+
+QUERY_CONSULTA_RESERVA_POR_ID = """
+SELECT h.id_hotel, r.id_reserva, dr.id_numero_reserva, dr.nombre 
+FROM hotel h 
+INNER JOIN reserva r ON h.id_hotel = r.id_hotel 
+INNER JOIN detalle_reserva dr ON r.id_reserva = dr.id_reserva 
+WHERE dr.id_numero_reserva = :numero_reserva
+"""
+
+QUERY_CANCELAR_RESERVA = """
+UPDATE detalle_reserva 
+SET activo = 0 
+WHERE id_numero_reserva = :numero_reserva
+"""
+
+QUERY_SELECT_POR_ID_RESERVA = """
+SELECT nombre, apellido, dni, inicio_reserva, fin_reserva
+FROM detalle_reserva
+WHERE id_numero_reserva = :numero_reserva
+"""
+
+QUERY_SELECT_HOTEL_POR_ESTRELLAS = """
+SELECT *
+FROM hotel h
+WHERE h.estrellas = :estrellas
+"""
+
+QUERY_SELECT_HOTEL_POR_UBICACION = """
+SELECT * 
+FROM hotel h
+WHERE h.provincia = :provincia
+"""
+
+engine = create_engine("mysql+mysqlconnector://root:root@localhost:3306/hoteles")
+
+def run_query(query, parameters=None):
+    with engine.connect() as conn:
+        result = conn.execute(text(query), parameters)
+        conn.commit()
+
+    return result
+
+def all_hoteles():
+    return run_query(QUERY_SELECT_HOTELES).fetchall()
+
+
+def hotel_by_id(id_hotel):
+    return run_query(QUERY_SELECT_HOTELES_ID, {'id_hotel': id_hotel}).fetchall()
+
+def hoteles_estrellas(estrellas):
+    return run_query(QUERY_SELECT_HOTEL_POR_ESTRELLAS, {'estrellas': estrellas}).fetchall()
+
+def hoteles_provincia(provincia):
+    return run_query(QUERY_SELECT_HOTEL_POR_UBICACION, {'provincia': provincia}).fetchall()
+
+
+
