d93dacbf0e9bb0f96278216fbe4c7da2f9bfca5e
ltkriger <lkriger@fi.uba.ar>
2024-11-14 14:07:39 -0300

    Se agregó la query para consultar reservas. Actualmente trae la información del hotel y la reserva, no de los servicios.

--- api/reservas.py
+++ api/reservas.py
@@ -21,6 +21,16 @@ SET activo = 0
 WHERE id_reserva = :id_reserva
 """
 
+QUERY_CONSULTAR_RESERVA = """
+SELECT dr.nombre, dr.apellido, dr.numero_reserva, dr.inicio_reserva, dr.fin_reserva,
+       h.nombre, h.provincia
+FROM detalle_reservas dr
+INNER JOIN reserva r ON dr.id_reserva = r.id_reserva
+INNER JOIN hoteles h ON r.id_hotel = h.id_hotel
+WHERE dr.numero_reserva = :numero_reserva AND dr.dni = :dni
+;
+"""
+
 engine = create_engine("mysql+mysqlconnector://root:root@localhost:3306/hoteles")
 
 def run_query(query, parameters=None):
@@ -50,3 +60,10 @@ def cancelar_reserva(id_reserva):
         "id_reserva": id_reserva,
     }
     run_query(QUERY_CANCELAR_RESERVA, reserva)
+
+def consultar_reserva(numero_reserva, dni):
+    reserva = {
+        "numero_reserva": numero_reserva,
+        "dni": dni
+    }
+    return run_query(QUERY_CONSULTAR_RESERVA, reserva).fetch_all()
